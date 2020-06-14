5facf1b487d592c95afd39496a5d0c05b38f3079
usuario <usuario@DESKTOP-8VGV6BL>
2020-06-14 12:20:12 -0600

    Menu Profesional

--- Ejercicios Clase/Form1.Designer.vb
+++ Ejercicios Clase/Form1.Designer.vb
@@ -22,10 +22,162 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.panelSuperior = New System.Windows.Forms.Panel()
+        Me.btnRestaurar = New System.Windows.Forms.PictureBox()
+        Me.btnMinimizar = New System.Windows.Forms.PictureBox()
+        Me.btnMaximizar = New System.Windows.Forms.PictureBox()
+        Me.btnCerrar = New System.Windows.Forms.PictureBox()
+        Me.panelMenu = New System.Windows.Forms.Panel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnMenu = New System.Windows.Forms.Button()
+        Me.panelContenedor = New System.Windows.Forms.Panel()
+        Me.timerOcultarMenu = New System.Windows.Forms.Timer(Me.components)
+        Me.timerMostrarMenu = New System.Windows.Forms.Timer(Me.components)
+        Me.panelSuperior.SuspendLayout()
+        CType(Me.btnRestaurar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnMinimizar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnMaximizar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnCerrar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.panelMenu.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'panelSuperior
+        '
+        Me.panelSuperior.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(80, Byte), Integer), CType(CType(200, Byte), Integer))
+        Me.panelSuperior.Controls.Add(Me.btnRestaurar)
+        Me.panelSuperior.Controls.Add(Me.btnMinimizar)
+        Me.panelSuperior.Controls.Add(Me.btnMaximizar)
+        Me.panelSuperior.Controls.Add(Me.btnCerrar)
+        Me.panelSuperior.Dock = System.Windows.Forms.DockStyle.Top
+        Me.panelSuperior.Location = New System.Drawing.Point(0, 0)
+        Me.panelSuperior.Name = "panelSuperior"
+        Me.panelSuperior.Size = New System.Drawing.Size(1000, 38)
+        Me.panelSuperior.TabIndex = 0
+        '
+        'btnRestaurar
+        '
+        Me.btnRestaurar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnRestaurar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnRestaurar.Image = CType(resources.GetObject("btnRestaurar.Image"), System.Drawing.Image)
+        Me.btnRestaurar.Location = New System.Drawing.Point(924, 0)
+        Me.btnRestaurar.Name = "btnRestaurar"
+        Me.btnRestaurar.Size = New System.Drawing.Size(39, 38)
+        Me.btnRestaurar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.btnRestaurar.TabIndex = 1
+        Me.btnRestaurar.TabStop = False
+        Me.btnRestaurar.Visible = False
+        '
+        'btnMinimizar
+        '
+        Me.btnMinimizar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnMinimizar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnMinimizar.Image = CType(resources.GetObject("btnMinimizar.Image"), System.Drawing.Image)
+        Me.btnMinimizar.Location = New System.Drawing.Point(885, 0)
+        Me.btnMinimizar.Name = "btnMinimizar"
+        Me.btnMinimizar.Size = New System.Drawing.Size(39, 38)
+        Me.btnMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.btnMinimizar.TabIndex = 1
+        Me.btnMinimizar.TabStop = False
+        '
+        'btnMaximizar
+        '
+        Me.btnMaximizar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnMaximizar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnMaximizar.Image = CType(resources.GetObject("btnMaximizar.Image"), System.Drawing.Image)
+        Me.btnMaximizar.Location = New System.Drawing.Point(924, 0)
+        Me.btnMaximizar.Name = "btnMaximizar"
+        Me.btnMaximizar.Size = New System.Drawing.Size(39, 38)
+        Me.btnMaximizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.btnMaximizar.TabIndex = 1
+        Me.btnMaximizar.TabStop = False
+        '
+        'btnCerrar
+        '
+        Me.btnCerrar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnCerrar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnCerrar.Image = CType(resources.GetObject("btnCerrar.Image"), System.Drawing.Image)
+        Me.btnCerrar.Location = New System.Drawing.Point(960, 0)
+        Me.btnCerrar.Name = "btnCerrar"
+        Me.btnCerrar.Size = New System.Drawing.Size(39, 38)
+        Me.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.btnCerrar.TabIndex = 0
+        Me.btnCerrar.TabStop = False
+        '
+        'panelMenu
+        '
+        Me.panelMenu.BackColor = System.Drawing.Color.FromArgb(CType(CType(26, Byte), Integer), CType(CType(32, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.panelMenu.Controls.Add(Me.Panel1)
+        Me.panelMenu.Controls.Add(Me.btnMenu)
+        Me.panelMenu.Dock = System.Windows.Forms.DockStyle.Left
+        Me.panelMenu.Location = New System.Drawing.Point(0, 38)
+        Me.panelMenu.Name = "panelMenu"
+        Me.panelMenu.Size = New System.Drawing.Size(220, 462)
+        Me.panelMenu.TabIndex = 1
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(80, Byte), Integer), CType(CType(200, Byte), Integer))
+        Me.Panel1.Location = New System.Drawing.Point(0, 36)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(5, 24)
+        Me.Panel1.TabIndex = 1
+        '
+        'btnMenu
+        '
+        Me.btnMenu.FlatAppearance.BorderSize = 0
+        Me.btnMenu.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(80, Byte), Integer), CType(CType(200, Byte), Integer))
+        Me.btnMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnMenu.ForeColor = System.Drawing.Color.White
+        Me.btnMenu.Image = CType(resources.GetObject("btnMenu.Image"), System.Drawing.Image)
+        Me.btnMenu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnMenu.Location = New System.Drawing.Point(4, 36)
+        Me.btnMenu.Name = "btnMenu"
+        Me.btnMenu.Size = New System.Drawing.Size(215, 24)
+        Me.btnMenu.TabIndex = 0
+        Me.btnMenu.Text = "Menu"
+        Me.btnMenu.UseVisualStyleBackColor = True
+        '
+        'panelContenedor
+        '
+        Me.panelContenedor.BackColor = System.Drawing.Color.FromArgb(CType(CType(49, Byte), Integer), CType(CType(66, Byte), Integer), CType(CType(82, Byte), Integer))
+        Me.panelContenedor.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.panelContenedor.Location = New System.Drawing.Point(220, 38)
+        Me.panelContenedor.Name = "panelContenedor"
+        Me.panelContenedor.Size = New System.Drawing.Size(780, 462)
+        Me.panelContenedor.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1000, 500)
+        Me.Controls.Add(Me.panelContenedor)
+        Me.Controls.Add(Me.panelMenu)
+        Me.Controls.Add(Me.panelSuperior)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.panelSuperior.ResumeLayout(False)
+        CType(Me.btnRestaurar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnMinimizar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnMaximizar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnCerrar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.panelMenu.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents panelSuperior As Panel
+    Friend WithEvents btnCerrar As PictureBox
+    Friend WithEvents panelMenu As Panel
+    Friend WithEvents panelContenedor As Panel
+    Friend WithEvents btnRestaurar As PictureBox
+    Friend WithEvents btnMinimizar As PictureBox
+    Friend WithEvents btnMaximizar As PictureBox
+    Friend WithEvents btnMenu As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents timerOcultarMenu As Timer
+    Friend WithEvents timerMostrarMenu As Timer
 End Class
--- Ejercicios Clase/Form1.vb
+++ Ejercicios Clase/Form1.vb
@@ -1,3 +1,57 @@
-﻿Public Class Form1
+﻿Imports System.Runtime.InteropServices
+Public Class Form1
+    <DllImport("user32.DLL", EntryPoint:="ReleaseCapture")>
+    Private Shared Sub ReleaseCapture()
+    End Sub
 
+    <DllImport("user32.DLL", EntryPoint:="SendMessage")>
+    Private Shared Sub SendMessage(ByVal hWnd As System.IntPtr, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As Integer)
+    End Sub
+
+
+    Private Sub btnCerrar_Click(sender As Object, e As EventArgs) Handles btnCerrar.Click
+        End
+
+    End Sub
+
+    Private Sub btnMaximizar_Click(sender As Object, e As EventArgs) Handles btnMaximizar.Click
+        btnMaximizar.Visible = False
+        btnRestaurar.Visible = True
+        Me.WindowState = FormWindowState.Maximized
+    End Sub
+
+    Private Sub btnMinimizar_Click(sender As Object, e As EventArgs) Handles btnMinimizar.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub btnRestaurar_Click(sender As Object, e As EventArgs) Handles btnRestaurar.Click
+        btnRestaurar.Visible = False
+        btnMaximizar.Visible = True
+        Me.WindowState = FormWindowState.Normal
+    End Sub
+
+    Private Sub PanelSuperior_MouseMove(sender As Object, e As MouseEventArgs) Handles PanelSuperior.MouseMove
+        ReleaseCapture()
+        SendMessage(Me.Handle, &H112&, &HF012&, 0)
+    End Sub
+
+    Private Sub btnMenu_Click(sender As Object, e As EventArgs) Handles btnMenu.Click
+        timerOcultarMenu.Enabled = True
+    End Sub
+
+    Private Sub timerOcultarMenu_Tick(sender As Object, e As EventArgs) Handles timerOcultarMenu.Tick
+        If PanelMenu.Width <= 60 Then
+            Me.timerOcultarMenu.Enabled = False
+        Else
+            Me.PanelMenu.Width = PanelMenu.Width - 20
+        End If
+    End Sub
+
+    Private Sub timerMostrarMenu_Tick(sender As Object, e As EventArgs) Handles timerMostrarMenu.Tick
+        If PanelMenu.Width >= 210 Then
+            Me.timerMostrarMenu.Enabled = False
+        Else
+            Me.PanelMenu.Width = PanelMenu.Width + 20
+        End If
+    End Sub
 End Class
